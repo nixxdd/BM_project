798977deed658fe8b429c72ab250545de578dbee
nixxdd <stefaniarosci2007@gmail.com>
2025-02-11 19:06:54 +0100

    Structure of the network is ready. Look for stimulus values and changes in the stimulus

--- BM_project/BM_project/constants.py
+++ BM_project/BM_project/constants.py
@@ -1,22 +1,22 @@
 """Parameters for balanced network"""
 
 # Excitatory Neuron Parameters
-E_NEURON_E_L = -60.0
+E_NEURON_E_L = -70.0
 E_NEURON_C_M = 250.0
 E_NEURON_TAU_M = 10.0
 E_NEURON_T_REF = 2.0
 E_NEURON_V_TH = -55.0
-E_NEURON_V_RESET = -60.0
+E_NEURON_V_RESET = -70.0
 E_NEURON_TAU_SYN_EX = 5.0
 E_NEURON_TAU_SYN_IN = 10.0
 
 # Inhibitory Neuron Parameters
-I_NEURON_E_L = -60.0
+I_NEURON_E_L = -70.0
 I_NEURON_C_M = 250.0
 I_NEURON_TAU_M = 10.0
 I_NEURON_T_REF = 2.0
 I_NEURON_V_TH = -55.0
-I_NEURON_V_RESET = -60.0
+I_NEURON_V_RESET = -70.0
 I_NEURON_TAU_SYN_EX = 5.0
 I_NEURON_TAU_SYN_IN = 10.0
 
@@ -26,8 +26,8 @@ THALAMIC_INPUT_WEIGHT = 15
 THALAMIC_INPUT_DELAY = 1.0
 
 # Population Parameters
-POP_SIZE_E = 200
-POP_SIZE_I = 70
+POP_SIZE_E = 25
+POP_SIZE_I = 15
 
 # Synapses Parameters
 DELAY_CONNECTION = 1.0
@@ -36,6 +36,12 @@ EXC_WEIGHT = 4.59
 INH_WEIGHT = -G * EXC_WEIGHT
 
 # Distribution of synaptic connections parameters
-GAUSSIAN_STD = 0.8
-DISTRIBUTION_THRESHOLD = 0.3
-MASK_RADIUS = 0.4
+GAUSSIAN_STD = 0.5
+GAUSSIAN_STD_LONG = 0.1
+DISTRIBUTION_THRESHOLD = 0.5
+MASK_RADIUS = 0.5
+
+# Distribution of short synaptic connections parameters
+GAUSSIAN_STD_SHORT = 0.8
+DISTRIBUTION_THRESHOLD_SHORT = 0.2
+MASK_RADIUS_SHORT = 0.5
